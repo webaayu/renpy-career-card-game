d2a8e69f16e5c4d0e00dd6944eab9267091d106f
webaayu <pratiksha28785@gmail.com>
2025-03-13 19:03:19 +0530

    updated app

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'careercard';
+var cacheName = 'teen-trailblazers';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
